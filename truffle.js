451246e5f283df29ecbfa6122099171a4ef5b30d
Taehyun Oh <gymnopedy01@gmail.com>
2022-01-19 23:23:55 +0900

    로컬 작업 중
    rpc 오류 나와서 보는중
    
    Error: Invalid JSON RPC response: {"id":5,"jsonrpc":"2.0"}
    
    강의까지는
    https://www.inflearn.com/course/%ED%81%B4%EB%A0%88%EC%9D%B4%ED%8A%BC/lecture/19257?volume=0.80&tab=curriculum&quality=auto
    3:48

--- truffle.js
+++ truffle.js
@@ -1,1 +1,19 @@
 // truffle.js config for klaytn.
+const PrivateKeyConnector = require('connect-privkey-to-provider')
+const NETWORK_ID = '1001'
+const GASLIMIT = '20000000'
+const URL = 'https://api.baobab.klaytn.net:8651'
+const PRIVATE_KEY = '0x6ea51ea8b81e0f775f0d7cd62eab7a92a46dc241957eea498f352c260d55f830'
+
+
+module.exports = {
+    networks: {
+        klaytn: {
+            provider: new PrivateKeyConnector(PRIVATE_KEY, URL), 
+            network_id: NETWORK_ID,
+            gas: GASLIMIT,
+            gasPrice: null,
+        }
+    }
+    
+}
